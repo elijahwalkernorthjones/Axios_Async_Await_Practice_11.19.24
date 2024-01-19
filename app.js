f0d1f3883c2728a424818d7e33a5d87b78758fac
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-01-19 09:37:08 -0500

    added some js

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// async function getStarWarsFilms(){
+//     console.log("starting")
+//     let res = await axios.get("https://swapi.dev/api/films/")
+//     for (let i of res.data.results){
+//         console.log(i.title)
+//     }
+    
+// }
+
+// getStarWarsFilms()
+
+
+//***************************** */
+
+
+//color chance async demo
