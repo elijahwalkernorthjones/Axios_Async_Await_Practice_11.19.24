5d4b44f1571666149cdf4d98a7b928b24c54fe26
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-01-20 11:09:12 -0500

    stillll twerkn

--- app.js
+++ app.js
@@ -15,3 +15,39 @@
 
 
 //color chance async demo
+
+// const h1 = document.getElementById("h1")
+
+// function changeColor(el, color){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             el.style.color = color;
+//             resolve()
+//         }, 1000)
+//     })
+// }
+
+// async function rainbowed(el){
+//     await changeColor(el, 'red')
+//     await changeColor(el, 'orange')
+//     await changeColor(el, 'yellow')
+//     await changeColor(el, 'green')
+//     await changeColor(el, 'blue')
+//     await changeColor(el, 'indigo')
+//     await changeColor(el, 'violet')
+// }
+
+
+// rainbowed(h1)
+
+
+//-------------------------------------------------------------------
+async function getStarWarsFilms(){
+    
+    let res = await axios.get("https://swapi.dev/api/films/")
+    for(let i of res.data.results){
+        console.log(i.opening_crawl)
+    }
+}
+
+getStarWarsFilms()
