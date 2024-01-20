c3c196d52c71f75e29fabe456a1944c62d2d3517
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-01-20 12:51:33 -0500

    sdkjhgs

--- app.js
+++ app.js
@@ -42,12 +42,74 @@
 
 
 //-------------------------------------------------------------------
-async function getStarWarsFilms(){
+// async function getStarWarsFilms(){
     
-    let res = await axios.get("https://swapi.dev/api/films/")
-    for(let i of res.data.results){
-        console.log(i.opening_crawl)
+//     let res = await axios.get("https://swapi.dev/api/films/")
+    
+// }
+
+// getStarWarsFilms()
+
+
+// using try and catch properly
+
+// async function getUser(user){
+//     try {
+//         let res = await axios.get(`https://api.github.com/users/${user}`)
+//         console.log(`${res.data.name}: ${res.data.bio}`)
+//     } catch (e) {
+//         console.log("User does not exist.")
+//     }
+
+// }
+
+// getUser('colt')
+
+
+//----------------------------------------
+//practice refactoring old JQuery Garbage lol
+
+// async function getPokemon(){
+//     let pokemonArr = []
+//     let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+
+//     for(let i=0; i<3; i++){
+//         pokemonArr.push(res.data.results[i].name)
+//     }
+//     console.log(pokemonArr)
+// }
+
+// getPokemon()
+
+async function getPoke(){
+    let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+    console.log(res.data.results[0].name)
+}
+    
+getPoke()
+
+//----------------------------------------
+// you can do async callbacks in PARALLEL... first calling the axios.get in batches, then saying "await" in a batch
+//then console.logging all of the promise responses. 
+//"axios.get, await, then do something with all responses at once"
+
+async function catchSomeOfEmParallel() {
+    try {
+        let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/`);
+
+        // Use Promise.all to fetch data in parallel
+        let [p1, p2, p3] = await Promise.all([
+            axios.get(res.data.results[0].url),
+            axios.get(res.data.results[1].url),
+            axios.get(res.data.results[2].url),
+        ]);
+
+        console.log(`The first pokemon is ${p1.data.name}`);
+        console.log(`The second pokemon is ${p2.data.name}`);
+        console.log(`The third pokemon is ${p3.data.name}`);
+    } catch (error) {
+        console.error("Error in catchSomeOfEmParallel:", error.message);
     }
 }
 
-getStarWarsFilms()
+catchSomeOfEmParallel();
